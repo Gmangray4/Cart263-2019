fd8f2eba1831af20a06199da008ead7b4431ed97
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2019-03-28 16:19:39 -0400

    Added test character

--- project3/js/script.js
+++ project3/js/script.js
@@ -9,3 +9,29 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let currentKey = "";
+let heldKey = "";
+
+let keys = {};
+
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+function setup(){
+
+}
+
+function draw(){
+checkKey();
+console.log(logkey);
+}
+
+function checkKey(){
+input.addEventListener('keydown', logKey);
+
+}
+
+function logKey(e) {
+  log.textContent == e;
+}
