b73257dc12bf575abd244d7064732e54b2c200e6
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2019-03-28 17:21:32 -0400

    Added Divs

--- project3/js/script.js
+++ project3/js/script.js
@@ -13,11 +13,6 @@ author, and this description to match your project!
 let currentKey = "";
 let heldKey = "";
 
-let keys = {};
-
-const input = document.querySelector('input');
-const log = document.getElementById('log');
-
 $(document).ready(setup);
 
 function setup(){
