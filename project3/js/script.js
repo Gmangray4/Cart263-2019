a45b4cf0eb333b5cd6ee35372ac2fbe60be2d527
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2019-04-18 21:29:31 -0400

    LastUpdate

--- project3/js/script.js
+++ project3/js/script.js
@@ -2,153 +2,49 @@
 
 /*****************
 
-Title of Project
-Author Name
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Title of Project: Meme Sabotage
+Author Name: Gordon Gray.
 
 ******************/
 
-///Ask about how to remove spsifc images with removeing the others.
-/// how to check how many image ids are left.
-// also how to check the window height and width
-
-// Key	Code
-// backspace	8
-// tab	9
-// enter	13
-// shift	16
-// ctrl	17
-// alt	18
-// pause/break	19
-// caps lock	20
-// escape	27
-// (space)	32
-// page up	33
-// page down	34
-// end	35
-// home	36
-// left arrow	37
-// up arrow	38
-// right arrow	39
-// down arrow	40
-// insert	45
-// delete	46
-// 0	48
-// 1	49
-// 2	50
-// 3	51
-// 4	52
-// 5	53
-// 6	54
-// 7	55
-// 8	56
-// 9	57
-// a	65
-// b	66
-// c	67
-// d	68
-// Key	Code
-// e	69
-// f	70
-// g	71
-// h	72
-// i	73
-// j	74
-// k	75
-// l	76
-// m	77
-// n	78
-// o	79
-// p	80
-// q	81
-// r	82
-// s	83
-// t	84
-// u	85
-// v	86
-// w	87
-// x	88
-// y	89
-// z	90
-// left window key	91
-// right window key	92
-// select key	93
-// numpad 0	96
-// numpad 1	97
-// numpad 2	98
-// numpad 3	99
-// numpad 4	100
-// numpad 5	101
-// numpad 6	102
-// numpad 7	103
-// Key	Code
-// numpad 8	104
-// numpad 9	105
-// multiply	106
-// add	107
-// subtract	109
-// decimal point	110
-// divide	111
-// f1	112
-// f2	113
-// f3	114
-// f4	115
-// f5	116
-// f6	117
-// f7	118
-// f8	119
-// f9	120
-// f10	121
-// f11	122
-// f12	123
-// num lock	144
-// scroll lock	145
-// semi-colon	186
-// equal sign	187
-// comma	188
-// dash	189
-// period	190
-// forward slash	191
-// grave accent	192
-// open bracket	219
-// back slash	220
-// close braket	221
-// single quote	222
 
 
+//Jquarly elements
 let $statDiv;
 let $gameWindow;
 let $p1Role;
 let $p2Role;
 let $roundNum;
 let $startButton;
-
+//chicks which player's turn it is.
 let player1Turn = false;
 let player2Turn = false;
 
+// the x-y cordinates for The Hacker player on screen.
 let hackerCoordinateX = 1;
 let hackerCoordinateY = 1;
-
+//Player's score/memecount
 let scoreP1 = 0;
 let scoreP2 = 0;
-
+// what round is it?
 let round = 0;
-
+//what does the voice to speech say
 let sentence = "";
 
-let turnInterval;
-
+// Ids array holder to chick the number of memes in the game.
 let arrayIds = [];
+// Ids array holder to chick the number of IncognitoWindows in the game.
 let arraysInC = [];
 
+//Which Incoginto is on screen.
+// all are set to false because there is no incognito window on the start of the game.
 let blockW1 = false;
 let blockW2 = false;
 let blockW3 = false;
 let blockW4 = false;
-
+// when a incognito window is called so that we can set a daley for when the next incognito can be called.
 let incognitoIsUsed = false;
-
+// all sounds
 let memeAddSound;
 let memeRemoveSound;
 let gameStartSound;
@@ -156,7 +52,7 @@ let incAddSound;
 let incRemoveSound;
 let bugSound;
 let gameOverSound;
-
+//all background songs
 let bgm1;
 let bgm2;
 let bgm3;
@@ -165,10 +61,10 @@ let bgm5;
 let bgm6;
 let bgm7;
 let menuBgm;
-
+// what song is currently in play?
 let currentSong;
 $(document).ready(setup);
-
+//allows clicking on the screen to be checked
 $(document).ready(function() {
   document.addEventListener("click", function() {
     console.log("click");
@@ -176,9 +72,9 @@ $(document).ready(function() {
 });
 
 function setup() {
+  // here the songs/bgm are being created
   menuBgm = new Pizzicato.Sound({
     source: 'file',
-    volume: 0.3,
     options: {
       path: 'assets/sounds/Music/Menu.mp3'
     }
@@ -227,13 +123,13 @@ function setup() {
       path: 'assets/sounds/Music/bgm7.mp3'
     }
   });
+  //Here is where the sound effects are being created
   memeAddSound = new Pizzicato.Sound({
     source: 'file',
     options: {
       path: 'assets/sounds/addMeme.wav'
     }
   });
-
   memeRemoveSound = new Pizzicato.Sound({
     source: 'file',
     options: {
@@ -271,19 +167,27 @@ function setup() {
       path: 'assets/sounds/gameOver.mp3'
     }
   });
-
+// all jQuery and id elements
   $startButton = $('#startButton');
   $statDiv = $('#statDiv');
   $gameWindow = $('#gameWindow');
   $p1Role = $('#p1Role');
   $p2Role = $('#p2Role');
   $roundNum = $('#roundNum');
+  // allows us to check which key is held down
   $(document).on("keydown", keyDown);
-  sentence = "Meme-ah-tahj";
+  // gives a sentence for the spaker to say that being the title
+  sentence = "Meme Sabotage";
+  // now speak
+  speak();
+  // this is the loding screen which is called here.
   lodingDiv();
 }
 
+// the round start and end function determines what happens when the first and seconed round starts
+
 function round1Start() {
+  // set the volume of the bgm so that the sound effects can be heard over the Music;
   bgm1.volume = 0.1;
   bgm2.volume = 0.1;
   bgm3.volume = 0.1;
@@ -291,50 +195,69 @@ function round1Start() {
   bgm5.volume = 0.1;
   bgm6.volume = 0.1;
   bgm7.volume = 0.1;
-  console.log("whahth");
+// remove the rules screen
   $("#statDiv").remove();
+// tell the system that it is now round 1
   round = 1;
+// start the coundown of the round;
   countdown();
   gameStartSound.play();
+// pick a background song
   bgmToPlay();
   menuBgm.stop();
 }
 
 function round1End() {
+  // add the number of memes left to the first player's score
   scoreP1 = arrayIds.length;
+  //display the round 1 over div/screen
   round1EndDiv();
+  //Remove all incognito windows
   incognitoRemoveAll();
+  // stop the current song
   bgmToStop();
   menuBgm.play();
 }
 
 function round2Start() {
+  // remove all memes from the last round
   for (var i = 0; i < arrayIds.length; i++) {
     document.getElementById(arrayIds[i]).remove();
     console.log(arrayIds[i]);
   }
 arrayIds = [];
+
+// set the text for player 1 to hacker
 p1Role.innerText = 'Hacker';
+// set the text for player 2 to Remover
 p2Role.innerText = 'Remover';
+// set the round display to round 2
 roundNum.innerText = '2';
+//remove the round 2 screen
 $("#endRound1Div").remove();
+// tell the system that it is now round 2
 round = 2;
+// stat the round countdown
 countdown();
 gameStartSound.play();
 bgmToPlay();
 menuBgm.stop();
-// bgmToPlay();
 }
 
 function round2End() {
+  // add the number of memes left to player 2's score
   scoreP2 = arrayIds.length;
+  //stop bgm
   bgmToStop();
+  //add the end game Results screen
   round2EndDiv();
   incognitoRemoveAll();
   gameOverSound.play();
   menuBgm.play();
 }
 
+// play again repeats most the code from round1Start()
+// but resets the score, resets on screen text and removes the end game screen
 function playAgain() {
   for (var i = 0; i < arrayIds.length; i++) {
     document.getElementById(arrayIds[i]).remove();
@@ -354,22 +277,30 @@ round = 1;
 menuBgm.stop();
 }
 
+// this cheicks all meme image creation fuction
 function createImage() {
   createImageW1();
   createImageW2();
   createImageW3();
   createImageW4();
 }
-
+//this creates a meme image on the top left desktop
 function createImageW1() {
+// the if statment makes sure that memes in this desktopwindow can only be made if
+//  the hacker is currently over the desktop of x 1 y 1 and also only allows memes
+// to be made if there is a incognito window over this desktop space.
 
+// most the code below was taken from this link below as it was very helpful for creatinng an image on a random area of the the window.
+//https://stackoverflow.com/questions/31843349/random-position-of-images
   if (hackerCoordinateX === 1 && hackerCoordinateY === 1 && blockW1 === true) {
+  //creates a fixed width and height for the images to appear on scrren
     let max_width = 400;
     let max_height = 150;
     let z = Math.floor((Math.random() * 100) + 1);
+    // i = which Meme image to be picked from the image folder
     let i = Math.floor((Math.random() * 118) + 1);
     let imgSize = Math.floor((Math.random() * 50) + 100);
-
+// picks a random spot to place the image
     let randomCoordinate = function() {
       var r = [];
       var x = Math.floor(Math.random() * max_width);
@@ -378,12 +309,13 @@ function createImageW1() {
       return r;
     };
     let xy = randomCoordinate();
-
+// how an image is created
     let img = document.createElement("img");
     img.src = "assets/images/memes/" + i + ".png";
     img.className = "memeImg";
     let id = "img" + i;
     img.id = id;
+    // adds the image to an id array
     arrayIds.push(id);
     img.height = imgSize;
     img.width = imgSize;
@@ -392,18 +324,17 @@ function createImageW1() {
     img.style.left = xy[0] + 'px';
     img.style.zIndex = z;
     img.addEventListener("click", function() {
+      // removes the image from image id array if it is easered
       let index = arrayIds.indexOf(img.id);
       if (index !== -1) arrayIds.splice(index, 1);
       img.remove();
       memeRemoveSound.play();
-      // textScoreP1.innerText = "Score = " + scoreP1;
     })
     window1.appendChild(img);
     memeAddSound.play();
-    // textScoreP1.innerText = "Score = " + scoreP1;
   }
 }
-
+//read CreateImageW1 to get an idea
 function createImageW2() {
   if (hackerCoordinateX === 2 && hackerCoordinateY === 1 && blockW2 === true) {
     let max_width = 400;
@@ -446,7 +377,7 @@ function createImageW2() {
     // textScoreP1.innerText = "Score = " + scoreP1;
   }
 }
-
+//read CreateImageW1 to get an idea
 function createImageW3() {
   if (hackerCoordinateX === 1 && hackerCoordinateY === 2 && blockW3 === true) {
     let max_width = 400;
@@ -489,7 +420,7 @@ function createImageW3() {
     // textScoreP1.innerText = "Score = " + scoreP1;
   }
 }
-
+//read CreateImageW1 to get an idea
 function createImageW4() {
   if (hackerCoordinateX === 2 && hackerCoordinateY === 2 && blockW4 === true) {
     let max_width = 400;
@@ -534,23 +465,12 @@ function createImageW4() {
   }
 }
 
-function changeKey() {
-  currentkey = possbleKeyList[Math.floor(Math.random() * possbleKeyList.length)];
-  console.log(currentkey);
-  turnDisplay();
-}
-
-function turnDisplay() {
-  if (player1Turn === true && player2Turn === false) {
-    textScoreP1.innerText = "Press Key: " + currentkey;
-    textScoreP2.innerText = "click to remove!";
-  } else if (player1Turn === false && player2Turn === true) {
-    textScoreP2.innerText = "Press Key: " + currentkey;
-    textScoreP1.innerText = "click to remove!";
-  }
-}
-
+//IncognitoWindows
 function blockWindow1() {
+  //creates an incognito if the hacker is over this desktop.
+  //also only lets an incognito be made every few seconeds after a pervous incognito has pervously been used
+  //simler to how a meme image above is created by instead for specific location on the screen souch as over a deststop window
+  //read CreateImageW1 to get an idea
   if (hackerCoordinateX === 1 && hackerCoordinateY === 1 && blockW1 === false && incognitoIsUsed === false) {
     let max_width = 0;
     let max_height = 5;
@@ -594,7 +514,6 @@ function blockWindow1() {
     incognitoTimer(incognitoTime);
   }
 }
-
 function blockWindow2() {
   if (hackerCoordinateX === 2 && hackerCoordinateY === 1 && blockW2 === false && incognitoIsUsed === false) {
     let max_width = 0;
@@ -639,7 +558,6 @@ function blockWindow2() {
     incognitoTimer(incognitoTime);
   }
 }
-
 function blockWindow3() {
   if (hackerCoordinateX === 1 && hackerCoordinateY === 2 && blockW3 === false && incognitoIsUsed === false) {
     let max_width = 0;
@@ -684,7 +602,6 @@ function blockWindow3() {
     incognitoTimer(incognitoTime);
   }
 }
-
 function blockWindow4() {
   if (hackerCoordinateX === 2 && hackerCoordinateY === 2 && blockW4 === false && incognitoIsUsed === false) {
     let max_width = 0;
@@ -730,7 +647,7 @@ function blockWindow4() {
   }
 }
 
-
+//the result screen for the end of the first round
 function round1EndDiv() {
   let div = document.createElement("div");
   div.className = "round1OverDiv";
@@ -745,13 +662,10 @@ function round1EndDiv() {
   div.style.position = "absolute"
   div.style.zIndex = 9999;
   document.getElementById("gameWindow").appendChild(div);
-  // div.style.top = placement("100px");
-  // div.style.left = placement("100px");
-  div.style.zIndex = 9999;
-  document.getElementById("gameWindow").appendChild(div);
+  // I used this to code the HTML right into the part of the code I needed
   div.innerHTML = '<button class="round2Button" onclick="round2Start()">Round 2</button>' + '<h3 class="r1p">Round 1 Results</h3>' + '<p class="r1p"> Player 1: <spam id="p1MemeCount">' + scoreP1 + '<spam> Memes Total!</p>' + '<p class="r1p"> Change roles: Player 1 is now the Hacker and Player 2 is the Remover</p>' + '<img class="r1EndImg" src="assets/images/euLock.jpg" alt="image">';
 }
-
+//the result screen for the end of the seconed round
 function round2EndDiv() {
   let div = document.createElement("div");
   div.className = "round2OverDiv";
@@ -766,12 +680,8 @@ function round2EndDiv() {
   div.style.position = "absolute"
   div.style.zIndex = 9999;
   document.getElementById("gameWindow").appendChild(div);
-  // div.style.top = placement("100px");
-  // div.style.left = placement("100px");
-  div.style.zIndex = 9999;
-  document.getElementById("gameWindow").appendChild(div);
 
-
+// sets up all the verables to the program to check who won
   let whoWins;
   let wholoses;
   if (scoreP1 < scoreP2) {
@@ -784,9 +694,9 @@ function round2EndDiv() {
     whoWins = "None";
     wholoses = "1 and 2";
   }
-  div.innerHTML = '<button class="round2Button" onclick="playAgain()"> Play again</button>' + '<h3 class="r1p">Game Results</h3>' + '<p class="r1p"> Player 1: <spam id="p1MemeCount">' + scoreP1 + '</spam> Memes Total!</p><p class="r1p"> Player 2: <spam id="p2MemeCount">' + scoreP2 + '</spam> Memes Total!</p> <p  class="r1p" >Player <spam> ' + wholoses + '</spam> has the most memes and is therefore busted for Meme possession.</p> <p  class="r1p">Player<spam> ' + whoWins + ' </spam>wins!</p>';
+  div.innerHTML = '<button class="round2Button" onclick="playAgain()"> Play again</button>' + '<h3 class="r1p">Game Results</h3>' + '<p class="r1p"> Player 1: <spam id="p1MemeCount">' + scoreP1 + '</spam> Memes Total!</p><p class="r1p"> Player 2: <spam id="p2MemeCount">' + scoreP2 + '</spam> Memes Total!</p> <p  class="r1p" >Player <spam> ' + wholoses + '</spam> has the most memes and is therefore busted for Meme possession.</p> <p  class="r1p">Player<spam> ' + whoWins + ' </spam>wins!</p>' + '<img class="r1EndImg" src="assets/images/go.jpg" alt="image">';
 }
-
+//the loding screen
 function lodingDiv() {
   let div = document.createElement("div");
   div.className = "loudingDev";
@@ -801,18 +711,13 @@ function lodingDiv() {
   div.style.position = "absolute"
   div.style.zIndex = 9999;
   document.getElementById("gameWindow").appendChild(div);
-  // div.style.top = placement("100px");
-  // div.style.left = placement("100px");
-  div.style.zIndex = 9999;
-  document.getElementById("gameWindow").appendChild(div);
-
-  div.innerHTML = '<p class="loading">Loading Meme Sabotage...<br><br> Please Hold your Memes Down and Thanks for Wating....</p>';
+  div.innerHTML = '<p class="loading">Loading Meme Sabotage! <br><br> Please Hold your Memes  and Thanks for Wating....</p>';
   loadTimeStart();
 }
 
+//if the on screen virus bug is clikced the systemwill remove all on screen memes
 function bugClicked() {
   document.getElementById("bug").onclick = function() {
-
     for (var i = 0; i < arrayIds.length; i++) {
       document.getElementById(arrayIds[i]).remove();
       console.log(arrayIds[i]);
@@ -821,24 +726,30 @@ function bugClicked() {
     bugSound.play();
   };
 }
-
+// used to remove all Incoginto windows on screen
 function incognitoRemoveAll(){
   blockW1 = false;
   blockW2 = false;
   blockW3 = false;
   blockW4 = false;
-
   for (var i = 0; i < arraysInC.length; i++) {
     document.getElementById(arraysInC[i]).remove();
   }
   arraysInC = [];
 }
 
+
+// Time fuctions
+// These timer fuctions were created by the link below and is remixed or editied by me to create the countdown needed for a few fuction
+// https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+
+// the Load time coundown 25 seconds
 function loadTimeStart() {
   let time = 25 * 1;
   loadTime(time);
 };
 
+// the duration of the load screen
 function loadTime(duration) {
   let timer = duration,
     minutes, seconds;
@@ -848,6 +759,7 @@ function loadTime(duration) {
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     if (--timer < 0) {
+      //rmoves the loading screen when the time is up
       clearInterval(timeforload);
       document.getElementById("loudDev").remove();
       menuBgm.play();
@@ -857,9 +769,7 @@ function loadTime(duration) {
   console.log(minutes);
 }
 
-
-
-// https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+// this is for time between rounds
 function gameTimer(duration, display) {
   let timer = duration,
     minutes, seconds;
@@ -884,7 +794,7 @@ function gameTimer(duration, display) {
   }, 1000);
   console.log(minutes);
 }
-
+// this is the daly time before the Hacker can create an incognito window again
 function incognitoTimer(duration) {
   incognitoIsUsed = true;
   let timer = duration,
@@ -902,7 +812,7 @@ function incognitoTimer(duration) {
   }, 1000);
   console.log(minutes);
 }
-
+// this is the coundown for the rounds annd is for 2 minutes
 function countdown() {
   sentence = "start";
   speak();
@@ -911,6 +821,7 @@ function countdown() {
   gameTimer(time, display);
 };
 
+//this fuction checks which window the hacker is own and is repstented with a yellow div
 function checkCoordinates() {
   if (hackerCoordinateX === 1 && hackerCoordinateY === 1) {
     document.getElementById("hackerS").style.margin = "0";
@@ -931,13 +842,12 @@ function checkCoordinates() {
 }
 
 function speak() {
-  // Set some random numbers for the voice's pitch and rate parameters for a bit of fun
+  // Set a radom pitch and leave the rate at 0.6
   let options = {
     pitch: Math.random(),
     rate: 0.6,
   };
-  // Use ResponsiveVoice to speak the string we generated, with UK English Male voice
-  // and the options we just specified.
+  // talk with the bristh voice
   responsiveVoice.speak(sentence, 'UK English Male', options);
 }
 
@@ -984,18 +894,19 @@ bgm7.stop();
 }
 
 function keyDown(e) {
-  // console.log("keyDown");
-  // console.log(heldKey);
+
   if (e.key === "q") {
     console.log("q");
     round2EndDiv();
   }
+  // key to make memes
   if (e.key === "m") {
     console.log("m");
     sentence = "meme";
     speak();
     createImage();
   }
+  // key to make an incognito
   if (e.key === "i") {
     console.log("i");
     blockWindow1();
@@ -1003,6 +914,7 @@ function keyDown(e) {
     blockWindow3();
     blockWindow4();
   }
+  // arrow keys to change windows the hacker is looking at
   // up arrow	38
   if (e.keyCode === 38) {
     console.log("38");
@@ -1036,11 +948,3 @@ function keyDown(e) {
     checkCoordinates();
   }
 }
-
-// function keyUp(e) {
-//   // console.log("KeyUp");
-//   if (heldKey === currentHeldKey) {
-//     heldKey = undefined;
-//     console.log("lol");
-//   }
-// }
